8fef5eed80c90f5d762c204fc823ff919f1c1223
anbuchelva <anbuchelva@gmail.com>
2020-12-17 13:57:32 +0530

    higher lower game - my version

--- 031-higher-lower-game/main.py
+++ 031-higher-lower-game/main.py
@@ -1,74 +1,63 @@
-# steps to process
-###################
-# import random
-# import art, game data
-# generate a random choice from the game data and store it in A
-# generate a random choice from the game data and store it in B - both requires a function
-# print the dictionary value in a sentance form - needs a function
-# compare the followers count betwen A and B
-# if the user's selection is correct, loop the game - by moving the B to A and compare it with new list
-# if the user's selection is incorrect, quit the game
-
 import random
 from art  import logo, vs
 from game_data import data
 from clear import clear
-# print(logo)
 
-def choose_data():
-    data_point = random.choice(data)
-    return data_point
-
-def person_to_text(person_dict, person):
+def person_to_text(person_dict):
+    """it converts the dictionary to readble text."""
     person_name = person_dict["name"]
     person_desc = person_dict["description"]
     person_cnty = person_dict["country"]
-    person_flwr = person_dict["follower_count"]
-    if person == 1:
-        print(f"Compare A: {person_name}, a {person_desc}, from {person_cnty}.")
-    else:
-        print(f"Against B: {person_name}, a {person_desc}, from {person_cnty}.")
-    return person_flwr
+    return person_name + ", a " + person_desc + ", from " + person_cnty +"."
 
-final_score = 0
-person_1_follwers = 0
-person_2_follwers = 1
-should_continue = True
-while should_continue:
-    while person_1_follwers != person_2_follwers:
-        if final_score == 0:
-            person_1_data = choose_data()
-            person_1_follwers = person_to_text(person_1_data,1)
-            # print(vs)
-            person_2_data = choose_data()
-            person_2_follwers = person_to_text(person_2_data,2)
-            print(f"info: person 1 followers {person_1_follwers} | person 2 followers {person_2_follwers}")
-        else:
-            person_1_follwers = person_to_text(person_x_data,1)
-            person_1_data = person_x_data
-            # print(vs)
-            person_2_data = choose_data()
-            person_2_follwers = person_to_text(person_2_data,2)
-
-    print(f"info: person 1 followers {person_1_follwers} | person 2 followers {person_2_follwers}")
-    user_selection = input("Who has more followers? Type 'A' or 'B': ")
-    
-    if user_selection == "A":
-        user_followers = person_1_follwers
-        user_data = person_1_data
-        other_followers = person_2_follwers
-    elif user_selection == "B":
-        user_followers = person_2_follwers
-        user_data = person_2_data
-        other_followers = person_1_follwers        
-
+def check_scores(user_guess,a_followers,b_followers):
+    """checks the followers between two set of dictionary"""
+    if user_guess == "a":
+        user_followers = a_followers
+        other_followers = b_followers
+    elif user_guess == "b":
+        user_followers = b_followers
+        # user_data = person_2_data
+        other_followers = a_followers
+    # print(f"U:{user_followers}, O: {other_followers}")
     if user_followers > other_followers:
-        final_score += 1
-        print(f"You're right! Current score: {final_score}.")
-        person_x_data = user_data
-        # print(person_x_data)
-        # print(final_score)
-        # print("-----------------------------------------------------------------------")
+        return True
     else:
-        print(f"Sorry, that's wrong. Final score: {final_score}")
+        return False
+
+should_continue = True
+final_score = 0
+while should_continue:
+    # clear previous contents
+    clear()
+    # print logo every time
+    print(logo)
+    if final_score > 0:
+        print(f"You're right! Current score: {final_score}.")
+        person_a = person_b        
+    # choose random dictionary from game data
+    else:
+        person_a = random.choice(data)
+
+    person_b = random.choice(data)
+    while person_a == person_b:
+        person_b = random.choice(data)
+
+    print(f"Compare A: {person_to_text(person_a)}")
+    print(vs)
+    print(f"Against B: {person_to_text(person_b)}")
+
+    # get followers count for each
+    a_followers = person_a["follower_count"]
+    b_followers = person_b["follower_count"]
+    print(f"A: {a_followers}, B: {b_followers}")
+    user_guess = input("Who has more followers? Type 'A' or 'B' or 'Q' to quit: ").lower()
+    if user_guess == "q":
+        break
+
+    # print(check_scores(user_guess,a_followers,b_followers))
+    if check_scores(user_guess,a_followers,b_followers):
+        final_score += 1
+    else:
+        print(f"\nSorry, that's wrong. Final score: {final_score}")
         should_continue = False
