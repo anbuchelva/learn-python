150b83791eb1f869cc7748e400a35eaffcfeecd2
anbuchelva <anbuchelva@gmail.com>
2020-12-24 21:52:08 +0530

    cosmetic changes in cofee machine program

--- 032-coffee-machine/main.py
+++ 032-coffee-machine/main.py
@@ -30,19 +30,22 @@ resources = {
     "coffee": 100,
 }
 
-def check_resources(cofee_flavor,ingredient):
+
+def check_resources(coffee_flavor, ingredient):
     ingredient_remain = resources[ingredient]
-    ingredient_requie = MENU[cofee_flavor]["ingredients"][ingredient]
-    # print(f"{ingredient} req: {ingredient_requie}, remain: {ingredient_remain}")
-    if ingredient_requie > ingredient_remain:
+    ingredient_require = MENU[coffee_flavor]["ingredients"][ingredient]
+    # print(f"{ingredient} req: {ingredient_require}, remain: {ingredient_remain}")
+    if ingredient_require > ingredient_remain:
         return False
-        # return f"Sorry, {ingredient} is not enough to make {cofee_flavor}"
+        # return f"Sorry, {ingredient} is not enough to make {coffee_flavor}"
     else:
         return True
 
+
 # def report(water_remain,milk_remain,coffee_remain,money_remain):
 def report():
-        print(f"Water: {water_remain}ml \nMilk: {milk_remain}ml \nCofee: {coffee_remain}g \nMoney: ${money_remain}")
+    print(f"Water: {water_remain}ml \nMilk: {milk_remain}ml \nCoffee: {coffee_remain}g \nMoney: ${money_remain}")
+
 
 turn_off_machine = False
 resources["money"] = 0.0
@@ -53,36 +56,36 @@ while not turn_off_machine:
     coffee_remain = resources["coffee"]
     money_remain = resources["money"]
 
-    cofee_flavor = input("What would you like? (espresso/latte/cappuccino): ").lower()
+    coffee_flavor = input("What would you like? (espresso/latte/cappuccino): ").lower()
 
-    if cofee_flavor == "espresso":
-        MENU[cofee_flavor]["ingredients"]["milk"] = 0
+    if coffee_flavor == "espresso":
+        MENU[coffee_flavor]["ingredients"]["milk"] = 0
 
-    if cofee_flavor == "off":
+    if coffee_flavor == "off":
         turn_off_machine = True
         print("Turning off coffee machine...")
-    elif cofee_flavor == "report":
+    elif coffee_flavor == "report":
         report()
         # check_resources()
     else:
-        water_req = MENU[cofee_flavor]["ingredients"]["water"]
-        milk_req = MENU[cofee_flavor]["ingredients"]["milk"]
-        coffee_req = MENU[cofee_flavor]["ingredients"]["coffee"]
-        money_req = MENU[cofee_flavor]["cost"]
+        water_req = MENU[coffee_flavor]["ingredients"]["water"]
+        milk_req = MENU[coffee_flavor]["ingredients"]["milk"]
+        coffee_req = MENU[coffee_flavor]["ingredients"]["coffee"]
+        money_req = MENU[coffee_flavor]["cost"]
 
         # print(f"water: {water_req}, milk: {milk_req}, Coffee: {coffee_req}, Money: {money_req}")
 
-        if not check_resources(cofee_flavor,"water"):
-            print(f"Sorry, water is not enough to make {cofee_flavor}")
+        if not check_resources(coffee_flavor, "water"):
+            print(f"Sorry, water is not enough to make {coffee_flavor}")
             turn_off_machine = True
-        elif not check_resources(cofee_flavor,"milk"):
-            print(f"Sorry, milk is not enough to make {cofee_flavor}")
+        elif not check_resources(coffee_flavor, "milk"):
+            print(f"Sorry, milk is not enough to make {coffee_flavor}")
             turn_off_machine = True
-        elif not check_resources(cofee_flavor,"coffee"):
-            print(f"Sorry, coffee is not enough to make {cofee_flavor}")
+        elif not check_resources(coffee_flavor, "coffee"):
+            print(f"Sorry, coffee is not enough to make {coffee_flavor}")
             turn_off_machine = True
         else:
-            # get inputs for the coints
+            # get inputs for the coins
             quarters = int(input("How many quarters?"))
             dimes = int(input("How many dimes? "))
             nickles = int(input("How many nickles? "))
@@ -99,8 +102,7 @@ while not turn_off_machine:
                 resources["water"] -= water_req
                 resources["milk"] -= milk_req
                 resources["coffee"] -= coffee_req
-                refund_money = round(amount_paid - money_req,2)
+                refund_money = round(amount_paid - money_req, 2)
                 print(f"Here is ${refund_money} in change.")
                 # update money in ledger
-                print(f"Your {cofee_flavor} is ready! Enjoy!")
-        
+                print(f"Your {coffee_flavor} is ready! Enjoy!")
