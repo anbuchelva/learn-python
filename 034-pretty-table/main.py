e34b1d56282e3fcefe2a08d72a0d9306427633be
anbuchelva <anbuchelva@gmail.com>
2020-12-25 22:51:54 +0530

    add pretty table sample

---
+++ 034-pretty-table/main.py
@@ -0,0 +1,12 @@
+from prettytable import PrettyTable
+
+table = PrettyTable()
+# table.field_names = ["Pokemon Name", "Type"]
+# table.add_row(["Pikachu", "Electric"])
+# table.add_row(["Squirtle", "Water"])
+# table.add_row(["Charmander", "Fire"])
+
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+table.align = "l"
+print(table)
