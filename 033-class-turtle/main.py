53d390070f2715d960ccf9d1580debb6ae6dda01
anbuchelva <anbuchelva@gmail.com>
2020-12-25 22:51:10 +0530

    add turtle class

---
+++ 033-class-turtle/main.py
@@ -0,0 +1,19 @@
+from turtle import Turtle, Screen
+timmy = Turtle()
+print(timmy)
+timmy.shape("turtle")
+timmy.color("coral")
+timmy.forward(100)
+timmy.right(90)
+timmy.forward(100)
+timmy.right(90)
+timmy.forward(100)
+timmy.right(90)
+timmy.forward(100)
+timmy.right(90)
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
+
+
