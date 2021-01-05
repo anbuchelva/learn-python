56c55474a36abb8379390bd92b70861314f1b3d0
anbuchelva <anbuchelva@gmail.com>
2021-01-05 23:20:45 +0530

    turtle cross game - my own version

--- 041-turtle-cross/cars.py
+++ 041-turtle-cross/cars.py
@@ -1,21 +1,25 @@
-from turtle import Turtle, Screen
+from turtle import Turtle
 import random
+
+COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
+STARTING_MOVE_DISTANCE = 5
+MOVE_INCREMENT = 10
 
 
 class Cars(Turtle):
     def __init__(self):
         super().__init__()
         self.shape("square")
-        self.color(self.random_color())
-        self.shapesize(stretch_len=3, stretch_wid=1)
+        self.color(random.choice(COLORS))
+        self.shapesize(stretch_len=2, stretch_wid=1)
         self.penup()
-        self.goto(400, random.randint(-160, 160))
-
-    def random_color(self):
-        colors = ["red", "orange", "blue", "purple", "green", "white"]
-        color = (random.choice(colors))
-        return color
+        self.goto(300, random.randint(-250, 250))
+        self.move_distance = 0
+        self.move_speed = 0
 
     def move(self):
-        # print(self.color)
-        self.goto(self.xcor()-10, self.ycor())
+        self.move_distance = STARTING_MOVE_DISTANCE + self.move_speed
+        self.goto(self.xcor() - self.move_distance, self.ycor())
+
+    def increase_speed(self, level):
+        self.move_speed = (level - 1) * MOVE_INCREMENT
--- 041-turtle-cross/main.py
+++ 041-turtle-cross/main.py
@@ -1,15 +1,42 @@
-from turtle import Screen, Turtle
-from cars import Cars
 import time
+from turtle import Screen
+from cars import Cars
+from player import Player
+from scoreboard import Scoreboard
 
 screen = Screen()
-screen.bgcolor("black")
-screen.setup(height=400, width=800)
 screen.tracer(0)
+screen.setup(width=600, height=600)
+scoreboard = Scoreboard()
+player = Player()
+
+car_list = []
+loop_count = 0
+
+screen.listen()
+screen.onkey(player.move, "Up")
 
 is_game_on = True
 while is_game_on:
+    loop_count += 1
+    if loop_count % 6 == 0:
+        new_car = Cars()
+        new_car.increase_speed(scoreboard.score)
+        car_list.append(new_car)
+
+    for car in car_list:
+        car.move()
+        if car.distance(player) < 25:
+            scoreboard.game_over()
+            is_game_on = False
+
     time.sleep(0.1)
     screen.update()
+
+    if player.level_up():
+        new_car = []
+        screen.update()
+        scoreboard.update_score()
+        player.level_up()
 
 screen.exitonclick()
