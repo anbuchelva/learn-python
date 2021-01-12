31d2823fc685be38b8972e7e39d37e666888f534
anbuchelva <anbuchelva@gmail.com>
2021-01-12 23:30:15 +0530

    find missing states - US states game

--- 045-US-states-game/main.py
+++ 045-US-states-game/main.py
@@ -13,12 +13,21 @@ correct_answer_count = 0
 correct_answers = []
 input_box_title = "Guess the State"
 states_data = pandas.read_csv("50_states.csv")
-
+all_states = states_data.state.tolist()
 while correct_answer_count != 50:
     answer_state = screen.textinput(title=input_box_title, prompt="What's another state's name?").title()
     # print(states_data.state)
     # print(answer_state in states_data.state.tolist())
-    if answer_state in states_data.state.tolist():
+    if answer_state == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in correct_answers:
+                missing_states.append(state)
+        missing_states_df = pandas.DataFrame(missing_states)
+        missing_states_df.to_csv("missing_states.csv")
+        break
+
+    if answer_state in all_states:
         state_name = State_Name()
         state_x = int(states_data[states_data.state == answer_state].x)
         state_y = int(states_data[states_data.state == answer_state].y)
@@ -28,4 +37,4 @@ while correct_answer_count != 50:
             correct_answers.append(answer_state)
     input_box_title = f"{correct_answer_count}/50 States Correct"
 
-screen.exitonclick()
+
