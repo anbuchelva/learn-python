90a35e7d54c9446ee3276d1e7e4de57891357ad3
anbuchelva <anbuchelva@gmail.com>
2021-03-05 19:05:35 +0530

    error handling on Nata Alphabets project

--- 051-NATO-alphabet/main.py
+++ 051-NATO-alphabet/main.py
@@ -3,11 +3,18 @@ import pandas
 alphabet_data = pandas.read_csv("nato_phonetic_alphabet.csv")
 alphabet_df = pandas.DataFrame(alphabet_data)
 
-# TODO 1. Create a dictionary in this format {"A": "Alfa", "B": "Bravo"}:
 alphabet_dict = {row["letter"]: row["code"] for (index, row) in alphabet_df.iterrows()}
-# print(alphabet_dict)
 
-# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-user_input_word = input("Enter a word: ").upper()
-nato_alphabet = [alphabet_dict[letter] for letter in user_input_word]
-print(nato_alphabet)
+
+def generate_nato_alphabet():
+    user_input_word = input("Enter a word: ").upper()
+    try:
+        nato_alphabet = [alphabet_dict[letter] for letter in user_input_word]
+    except KeyError:
+        print("Sorry! Only the letters in alphabets are expected.")
+        generate_nato_alphabet()
+    else:
+        print(nato_alphabet)
+
+
+generate_nato_alphabet()
