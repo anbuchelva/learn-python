cbf3ee377d18c87a9917b1f3901332b13a348935
anbuchelva <anbuchelva@gmail.com>
2021-09-02 22:03:58 +0530

    kanye quotes change font color

--- 063-kanye-quotes/main.py
+++ 063-kanye-quotes/main.py
@@ -19,7 +19,7 @@ canvas = Canvas(width=300, height=414)
 background_img = PhotoImage(file="background.png")
 canvas.create_image(150, 207, image=background_img)
 quote_text = canvas.create_text(150, 207, text="The quote appear here", width=250,
-                                font=("Arial", 15, "bold"), fill="white")
+                                font=("Arial", 15, "bold"), fill="black")
 canvas.grid(row=0, column=0)
 
 kanye_img = PhotoImage(file="kanye.png")
