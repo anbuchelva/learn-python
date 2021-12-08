b7a7cb9aacac6d9ab80da9dc5f640ad47799838c
anbuchelva <anbuchelva@gmail.com>
2021-12-08 16:45:29 +0530

    beautiful soup: top 100 movies to watch

--- 072-beautiful-soup/main.py
+++ 072-beautiful-soup/main.py
@@ -16,7 +16,13 @@ movie_list = []
 for movie in movies:
     movie_list.append(movie.text)
 
-total_movies = len(movie_list)
-for i in range(total_movies):
-    i += 1
-    print(movie_list[total_movies - i])
+movie_list = movie_list[::-1]
+# Other option to sort the list 
+# total_movies = len(movie_list)
+# for i in range(total_movies):
+#     i += 1
+#     print(movie_list[total_movies - i])
+
+with open("top100-movies.txt","w") as file:
+    for movie in movie_list:
+        file.write(f"{movie}\n")
